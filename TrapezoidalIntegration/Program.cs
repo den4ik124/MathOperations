208054c5383c5f8d9e73f6c68563be6aaf41cbf3
den4ik124 <den4ik124@gmail.com>
2021-08-09 17:21:51 +0300

    Добавил метод Симпсона для численного вычисления интеграла функции

--- TrapezoidalIntegration/Program.cs
+++ TrapezoidalIntegration/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,34 @@ namespace TrapezoidalIntegration
     {
         private static void Main()
         {
-            //var result = FunctionArea(2, 5, 3, (x) => 1.0 / Math.Log(x));
-            var result = FunctionArea(0, 5, 500, (x) => 50 - Math.Pow(x, 2));
+            //var result = FunctionArea(0, 5, 500, (x) => 50 - Math.Pow(x, 2));
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = FunctionArea(2, 5, 300, (x) => 1.0 / Math.Log(x));
+            stopwatch.Stop();
+            Console.WriteLine($"Метод трапеции\nЗатрачено: {stopwatch.ElapsedMilliseconds} ms.");
+            //var result2 = FunctionAreaSimpson(0, 5, 2, (x) => 50 - Math.Pow(x, 2));
+            stopwatch.Start();
+            var result2 = FunctionAreaSimpson(2, 5, 10, (x) => 1.0 / Math.Log(x));
+            stopwatch.Stop();
+            Console.WriteLine($"Метод Симпсона\nЗатрачено: {stopwatch.ElapsedMilliseconds} ms.");
             Console.WriteLine(result);
             Console.WriteLine();
+        }
+
+        private static double FunctionAreaSimpson(double start, double end, int amount, Func<double, double> functionCalc)
+        {
+            double step = (end - start) / amount;
+            double area = 0;
+            double[] args = new double[amount + 1];
+            args[0] = start;
+            for (int i = 1; i < args.Length; i++)
+            {
+                args[i] = args[i - 1] + step;
+                double meanValue = (args[i] + args[i - 1]) / 2;
+                area += (args[i] - args[i - 1]) / 6 * (functionCalc(args[i - 1]) + 4 * functionCalc(meanValue) + functionCalc(args[i]));
+            }
+            return area;
         }
 
         private static double FunctionArea(double start, double end, int amount, Func<double, double> functionCalc)
